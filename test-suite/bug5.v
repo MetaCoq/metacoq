e17b5f67195bbc25073fd35d8151683737e621e8
Gregory Malecha <gmalecha@cs.harvard.edu>
2015-06-15 22:19:50 -0700

    bug reports from Jason.

---
+++ test-suite/bug5.v
@@ -0,0 +1,4 @@
+Require Import Template.Template.
+
+Quote Recursively Definition aterm := Ast.term.
+Time Quote Recursively Definition aterm' := aterm.
---
+++ test-suite/bug6.v
@@ -0,0 +1,6 @@
+(** Reported by Randy Pollack **)
+Require Import Template.Template.
+
+Definition I (t:Type) (x:t) : t := x.
+Definition II := I (forall t:Type, t -> t) I.
+Quote Definition qII := Eval compute in II.

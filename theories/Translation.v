bc9bf0486870488d3c325970c8cde3eeeb16e3bc
Théo Winterhalter <isheeft@gmail.com>
2018-01-11 17:50:54 +0100

    Partial relation for translated expressions
    The definition is not the difficulty so we should skip ahead.

--- theories/Translation.v
+++ theories/Translation.v
@@ -7,26 +7,27 @@ From Template Require Import Ast SAst LiftSubst SLiftSubst Typing ITyping XTypin
 
 Section Translation.
 
+Open Scope x_scope.
+
 (* Transport in the target *)
-(* We want transport to be a symbolic construction, do we need to be able to
-   match on it? If so, we'll need to change the target syntax as well... *)
-Context (transport : sort -> term -> term -> term -> term -> term).
-Context (tEq : sort -> term -> term -> term -> term).
+(* Maybe it should be added to the common syntax *)
+Context (transport : sort -> sterm -> sterm -> sterm -> sterm -> sterm).
+Context (tEq : sort -> sterm -> sterm -> sterm -> sterm).
 Context (type_transport :
   forall Σ Γ s T1 T2 p t ,
-    Σ ;;; Γ |-- p : tEq (succ_sort s) (tSort s) T1 T2 ->
+    Σ ;;; Γ |-- p : sEq (succ_sort s) (sSort s) T1 T2 ->
     Σ ;;; Γ |-- t : T1 ->
     Σ ;;; Γ |-- transport s T1 T2 p t : T2
 ).
 
 (*! Relation for translated expressions *)
-Inductive trel (E : list (nat * nat)) : term -> term -> Prop :=
+Inductive trel (E : list (nat * nat)) : sterm -> sterm -> Prop :=
 | trel_assumption x y :
     In (x,y) E ->
-    trel E (tRel x) (tRel y)
+    trel E (sRel x) (sRel y)
 
 | trel_Rel x :
-    trel E (tRel x) (tRel x)
+    trel E (sRel x) (sRel x)
 
 | trel_transportl t1 t2 s T1 T2 p :
     trel E t1 t2 ->
@@ -36,7 +37,27 @@ Inductive trel (E : list (nat * nat)) : term -> term -> Prop :=
     trel E t1 t2 ->
     trel E t1 (transport s T1 T2 p t2)
 
-(* It seems I was mistaken. The target does need annotations. *)
+| trel_Prod n1 n2 A1 A2 B1 B2 :
+    trel E A1 A2 ->
+    trel E B1 B2 ->
+    trel E (sProd n1 A1 B1) (sProd n2 A2 B2)
+
+| trel_Eq s A1 A2 u1 u2 v1 v2 :
+    trel E A1 A2 ->
+    trel E u1 u2 ->
+    trel E v1 v2 ->
+    trel E (sEq s A1 u1 v1) (sEq s A2 u2 v2)
+
+| trel_Sort s :
+    trel E (sSort s) (sSort s)
+
+| trel_Lambda n1 n2 A1 A2 B1 B2 u1 u2 :
+    trel E A1 A2 ->
+    trel E B1 B2 ->
+    trel E u1 u2 ->
+    trel E (sLambda n1 A1 B1 u1) (sLambda n2 A2 B2 u2)
+
+(* TODO *)
 .
 
 End Translation.

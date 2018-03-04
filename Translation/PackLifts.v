fce6594ec982705381f9e6f0c47204deacb7d6a4
Théo Winterhalter <isheeft@gmail.com>
2018-03-04 16:55:05 +0100

    Correct form of lif_llift4

--- Translation/PackLifts.v
+++ Translation/PackLifts.v
@@ -280,95 +280,25 @@ Proof.
       reflexivity.
 Defined.
 
-(*
-lift0 (S n)
-    (llift0 (#|Γm| - S (n - #|llift_context #|Γm| Δ|)) t) =
-  llift #|Γm| #|Δ|
-    (lift0 (S n) t)
-
-i = S n
-j = #|Γm| - S (n - #|llift_context #|Γm| Δ|)
-  = #|Γm| - S n + #|Δ|
-l = #|Γm|
-k = #|Δ|
-
-i + j = #|Γm| + #|Δ|
-      = k + l
-
-#|Δ| <= n
-n < #|Δ| + #|Γm|
-*)
 Lemma lift_llift4 :
   forall {t i j k},
     k < i ->
     i <= k + j ->
-    lift i 0 (llift (j - i + k) 0 t) = llift j k (lift i 0 t).
+    lift i 0 (llift (j - (i - k)) 0 t) = llift j k (lift i 0 t).
 Proof.
   intro t. induction t ; intros i j k h1 h2.
   { unfold llift at 1.
     case_eq (n <? 0) ; intro e ; bprop e ; try omega.
-    case_eq (n <? 0 + (j - i + k)) ; intro e1 ; bprop e1 ; try omega.
+    case_eq (n <? 0 + (j - (i - k))) ; intro e1 ; bprop e1 ; try omega.
     - unfold lift. case_eq (0 <=? n) ; intro e3 ; bprop e3 ; try omega.
       unfold llift. case_eq (i + n <? k) ; intro e5 ; bprop e5 ; try omega.
       case_eq (i+n <? k + j) ; intro e7 ; bprop e7 ; try omega.
-      + reflexivity.
-      + (* Isn't there a contradiction ?
-           n < 0 + (j - i + k)
-           k + j <= i + n
-         *)
-Abort.
-
-Lemma lift_llift4 :
-  forall {t i j k},
-    k < i ->
-    i <= k + i + j + k ->
-    lift i 0 (llift j 0 t) = llift (i+j+k) k (lift i 0 t).
-Proof.
-  intro t. induction t ; intros i j k h1 h2.
-  { unfold llift at 1.
-    case_eq (n <? 0) ; intro e ; bprop e ; try omega.
-    case_eq (n <? 0+j) ; intro e1 ; bprop e1 ; try omega.
-    - unfold lift. case_eq (0 <=? n) ; intro e3 ; bprop e3 ; try omega.
-      unfold llift. case_eq (i+n <? k) ; intro e5 ; bprop e5 ; try omega.
-      case_eq (i+n <? k+(i+j+k)) ; intro e7 ; bprop e7 ; try omega.
       reflexivity.
     - unfold lift. case_eq (0 <=? n) ; intro e3 ; bprop e3 ; try omega.
       unfold llift. case_eq (i+n <? k) ; intro e5 ; bprop e5 ; try omega.
-      case_eq (i+n <? k+(i+j+k)) ; intro e7 ; bprop e7 ; try omega.
-      +
-(* Problem: This doesn't even seem to hold for 0!
-   I will certainly have a hard time generalising it.
-   We first need to figure out a true version.
- *)
-Abort.
-
-
-Lemma lift_llift4 :
-  forall {t i j k l m},
-    k < i ->
-    m <= l ->
-    lift i l (llift j m t) = llift (i+j+k) (k+l) (lift i l t).
-Proof.
-  intro t ; induction t ; intros i j k l m h1 h2.
-  all: try (cbn ; f_equal ;
-            try replace (S (S (k + l))) with (k + (S (S l)))%nat by omega ;
-            try replace (S (k + l)) with (k + (S l))%nat by omega ;
-            easy).
-  unfold llift at 1.
-  case_eq (n <? m) ; intro e ; bprop e ; try omega.
-  - cbn. case_eq (l <=? n) ; intro e1 ; bprop e1 ; try omega.
-    + unfold llift. case_eq (i+n <? k+l) ; intro e3 ; bprop e3 ; try omega.
-      case_eq (i+n <? k+l+(i+j+k)) ; intro e5 ; bprop e5 ; try omega.
-      * case_eq (n <? k+l) ; intro e7 ; bprop e7 ; try omega.
-        reflexivity.
-      * case_eq (n <? k+l) ; intro e7 ; bprop e7 ; try omega.
-        reflexivity.
-  - case_eq (n <? m+j) ; intro e1 ; bprop e1 ; try omega.
-    + cbn. case_eq (l <=? n) ; intro e3 ; bprop e3 ; try omega.
-      * unfold llift. case_eq (i+n <? k+l) ; intro e5 ; bprop e5 ; try omega.
-        case_eq (i+n <? k+l+(i+j+k)) ; intro e7 ; bprop e7 ; try omega.
-        reflexivity.
-      * unfold llift. case_eq (n <? k+l) ; intro e5 ; bprop e5 ; try omega.
+      case_eq (i+n <? k+j) ; intro e7 ; bprop e7 ; try omega.
+      reflexivity.
+  }
 Abort.
 
 Lemma lift_llift5 :

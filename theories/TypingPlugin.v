59cf55d1f01c2f3c5900e879d8cd88d9e580c543
SimonBoulier <simon.boulier@ens-rennes.fr>
2018-03-19 15:56:39 +0100

    WIP: trying fixing the extraction

--- theories/TypingPlugin.v
+++ theories/TypingPlugin.v
@@ -2,7 +2,7 @@ From Template Require Import Ast AstUtils Typing Checker.
 
 Require Import ExtrOcamlBasic.
 Require Import ExtrOcamlString.
-Require Import ExtrOcamlZInt.
+(* Require Import ExtrOcamlZInt. *)
 
 Set Extraction Optimize.
 
@@ -10,13 +10,4 @@ Extraction Blacklist uGraph univ Ast AstUtils String List Nat Typing.
 
 Set Warnings "-extraction-opaque-accessed".
 
-Extraction Library Specif.
-Extraction Library PeanoNat.
-Extraction Library Sumbool.
-Extraction Library Induction.
-Extraction Library LiftSubst.
-Extraction Library UnivSubst.
-Extraction Library Typing.
-Extraction Library utils.
-Extraction Library AstUtils.
-Extraction Library Checker.
+Recursive Extraction Library Checker.

190faa830c9ff5f7e2b86dfa4e0c5d3a9c088f4a
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2018-04-18 17:22:09 +0200

    Add a MetaTheory file with conjectures of the main metatheoretical properties

---
+++ theories/MetaTheory.v
@@ -0,0 +1,24 @@
+(* Distributed under the terms of the MIT license.   *)
+
+From Coq Require Import Bool String List Program BinPos Compare_dec Omega.
+From Template Require Import Template utils Ast univ Induction LiftSubst UnivSubst Typing.
+From Template Require AstUtils.
+Require Import String.
+Local Open Scope string_scope.
+Set Asymmetric Patterns.
+
+(** Use a coercion for this common projection of the global context. *)
+Definition fst_ctx : global_context -> global_declarations := fst.
+Coercion fst_ctx : global_context >-> global_declarations.
+
+(** The subject reduction property of the system: *)
+
+Conjecture subject_reduction : forall (Σ : global_context) Γ t u T,
+    Σ ;;; Γ |- t : T -> red Σ Γ t u -> Σ ;;; Γ |- u : T.
+
+(** Weak Normalization: every term has a normal form *)
+
+Definition normal (Σ : global_context) Γ t := ~ exists u, red1 Σ Γ t u.
+
+Conjecture weak_normalization : forall (Σ : global_context) Γ t T,
+    Σ ;;; Γ |- t : T -> exists u, red Σ Γ t u /\ normal Σ Γ u.

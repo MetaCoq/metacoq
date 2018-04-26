d022017bf8e5118ef9107f60444cd42e563b4679
Yannick Forster <forster@ps.uni-saarland.de>
2018-04-26 17:37:33 +0200

    Extended reductionstrategy by unfold option

--- template-coq/theories/Ast.v
+++ template-coq/theories/Ast.v
@@ -253,7 +253,7 @@ Definition program : Type := global_declarations * term.
 (** Reduction strategy to apply, beware [cbv], [cbn] and [lazy] are _strong_. *)
 
 Inductive reductionStrategy : Set :=
-  cbv | cbn | hnf | all | lazy.
+  cbv | cbn | hnf | all | lazy | unfold (i : ident).
 
 Definition typed_term := {T : Type & T}.
 Definition existT_typed_term a t : typed_term := @existT Type (fun T => T) a t.

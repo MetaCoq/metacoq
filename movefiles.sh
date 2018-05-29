78aef841578b230f070ca26cce2ad958fc89fc3b
SimonBoulier <simon.boulier@ens-rennes.fr>
2018-05-29 13:54:01 +0200

    Comment in movefiles.sh

--- movefiles.sh
+++ movefiles.sh
@@ -2,10 +2,10 @@
 
 cd template-coq
 
-shopt -s nullglob # make the for loop doing nothnig when there is no *.ml* files
+shopt -s nullglob # make the for loop do nothnig when there is no *.ml* files
 
 for i in *.ml*; do
-    j=`echo $i | cut -b 1 | tr '[:upper:]' '[:lower:]'`;
-    k=`echo $i | cut -b 2-`;
+    j=`echo $i | cut -b 1 | tr '[:upper:]' '[:lower:]'`; # the first letter of file name is put in lowercase
+    k=`echo $i | cut -b 2-`; # the rest is untouched
     mv $i ../checker/src/$j$k;
 done

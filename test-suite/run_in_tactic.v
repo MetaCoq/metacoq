010820ad3fd5ad6055eed7d565478cde2d755cdf
Yannick Forster <forster@ps.uni-saarland.de>
2018-07-19 19:02:26 +0200

    Added failing test as well

--- test-suite/run_in_tactic.v
+++ test-suite/run_in_tactic.v
@@ -5,4 +5,7 @@ Import ListNotations MonadNotation.
 Goal True.
   let k x := pose (y := x) in
   run_template_program (tmPrint "test" ;; tmQuote plus) k.
+
+  Fail let k x := pose (y := x) in
+  run_template_program (tmLemma "test" nat) k.
 Abort.

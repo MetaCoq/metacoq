274a73758d430060a4df28618345a22b2b537ff4
Théo Winterhalter <isheeft@gmail.com>
2018-12-17 17:20:24 +0100

    Fix PCUICAll (?)

--- pcuic/theories/PCUICAll.v
+++ pcuic/theories/PCUICAll.v
@@ -1,16 +1,20 @@
 (* Distributed under the terms of the MIT license.   *)
 
-From Template Require Export
-     utils         (* Utility functions *)
-     monad_utils   (* Monadic notations *)
-     Ast           (* The term AST *)
-     TemplateMonad (* The TemplateMonad *)
-     AstUtils      (* Utilities on the AST *)
-     Loader        (* Declaration of the Template Coq plugin *)
-     Induction     (* Induction *)
-     LiftSubst     (* Lifting and substitution for terms *)
-     WeakSubst     (* Weakening and substitution lemmas *)
-     UnivSubst     (* Substitution of universe instances *)
-     Typing        (* Typing judgment *)
-     Checker       (* Partial typechecker implementation *)
-     Retyping      (* Fast retyping judgment *).
+From PCUIC Require Export
+     PCUICAst
+     PCUICAstUtils
+     PCUICInduction
+     PCUICLiftSubst
+     PCUICUnivSubst
+     PCUICTyping
+     PCUICWeakeningEnv
+     PCUICClosed
+     PCUICWeakening
+     PCUICSubstitution
+     PCUICMetaTheory
+     PCUICWcbvEval
+     PCUICChecker
+     PCUICRetyping
+     PCUICAAst
+     TemplateToPCUIC
+     PCUICAAst.

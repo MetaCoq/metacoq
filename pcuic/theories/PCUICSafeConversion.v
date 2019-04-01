d035060b3b78285cc9ccb1649b9e6ef6f90e447f
Théo Winterhalter <isheeft@gmail.com>
2019-04-01 14:06:56 +0200

    Define new type of position and a well-order on them

--- pcuic/theories/PCUICSafeConversion.v
+++ pcuic/theories/PCUICSafeConversion.v
@@ -53,8 +53,200 @@ Section Conversion.
 
   Set Equations With UIP.
 
-  Derive Subterm for term.
-  Derive NoConfusion NoConfusionHom Subterm EqDec for stack.
+  Derive NoConfusion NoConfusionHom EqDec for stack.
+
+  (* A choice is a local position.
+     We redefine positions in a non dependent way to make it more practical.
+   *)
+  Inductive choice :=
+  | app_l
+  | app_r
+  | case_c
+  | lam_ty
+  | lam_tm
+  | prod_l
+  | prod_r.
+
+  Derive NoConfusion NoConfusionHom EqDec for choice.
+
+  Definition position := list choice.
+
+  (* Fixpoint validpos t (p : position) {struct t} := *)
+  (*   match t with *)
+  (*   | tApp u v => *)
+  (*     match p with *)
+  (*     |  *)
+
+  Fixpoint validpos t (p : position) {struct p} :=
+    match p with
+    | [] => true
+    | c :: p =>
+      match c, t with
+      | app_l, tApp u v => validpos u p
+      | app_r, tApp u v => validpos v p
+      | case_c, tCase indn pr c brs => validpos c p
+      | lam_ty, tLambda na A t => validpos A p
+      | lam_tm, tLambda na A t => validpos t p
+      | prod_l, tProd na A B => validpos A p
+      | prod_r, tProd na A B => validpos B p
+      | _, _ => false
+      end
+    end.
+
+  Definition pos (t : term) := { p : position | validpos t p = true }.
+
+  Arguments exist {_ _} _ _.
+
+  Definition dapp_l u v (p : pos u) : pos (tApp u v) :=
+    exist (app_l :: ` p) (proj2_sig p).
+
+  Definition dapp_r u v (p : pos v) : pos (tApp u v) :=
+    exist (app_r :: ` p) (proj2_sig p).
+
+  Definition dcase_c indn pr c brs (p : pos c) : pos (tCase indn pr c brs) :=
+    exist (case_c :: ` p) (proj2_sig p).
+
+  Definition dlam_ty na A t (p : pos A) : pos (tLambda na A t) :=
+    exist (lam_ty :: ` p) (proj2_sig p).
+
+  Definition dlam_tm na A t (p : pos t) : pos (tLambda na A t) :=
+    exist (lam_tm :: ` p) (proj2_sig p).
+
+  Definition dprod_l na A B (p : pos A) : pos (tProd na A B) :=
+    exist (prod_l :: ` p) (proj2_sig p).
+
+  Definition dprod_r na A B (p : pos B) : pos (tProd na A B) :=
+    exist (prod_r :: ` p) (proj2_sig p).
+
+  Inductive positionR : position -> position -> Prop :=
+  | positionR_app_lr p q : positionR (app_r :: p) (app_l :: q)
+  | positionR_deep c p q : positionR p q -> positionR (c :: p) (c :: q)
+  | positionR_root c p : positionR (c :: p) [].
+
+  Derive Signature for positionR.
+
+  Definition posR {t} (p q : pos t) : Prop :=
+    positionR (` p) (` q).
+
+  Lemma posR_Acc :
+    forall t p, Acc (@posR t) p.
+  Proof.
+    assert (forall na A B p, Acc posR p -> Acc posR (dprod_l na A B p)) as Acc_prod_l.
+    { intros na A B p h.
+      induction h as [p ih1 ih2].
+      constructor. intros [q e] h.
+      dependent destruction h. cbn in e.
+      eapply (ih2 (exist p0 e)). assumption.
+    }
+    assert (forall na A B p, Acc posR p -> Acc posR (dprod_r na A B p)) as Acc_prod_r.
+    { intros na A B p h.
+      induction h as [p ih1 ih2].
+      constructor. intros [q e] h.
+      dependent destruction h. cbn in e.
+      eapply (ih2 (exist p0 e)). assumption.
+    }
+    assert (forall na A t p, Acc posR p -> Acc posR (dlam_ty na A t p)) as Acc_lam_ty.
+    { intros na A t p h.
+      induction h as [p ih1 ih2].
+      constructor. intros [q e] h.
+      dependent destruction h. cbn in e.
+      eapply (ih2 (exist p0 e)). assumption.
+    }
+    assert (forall na A t p, Acc posR p -> Acc posR (dlam_tm na A t p)) as Acc_lam_tm.
+    { intros na A t p h.
+      induction h as [p ih1 ih2].
+      constructor. intros [q e] h.
+      dependent destruction h. cbn in e.
+      eapply (ih2 (exist p0 e)). assumption.
+    }
+    assert (forall u v p, Acc posR p -> Acc posR (dapp_r u v p)) as Acc_app_r.
+    { intros u v p h.
+      induction h as [p ih1 ih2].
+      constructor. intros [q e] h.
+      dependent destruction h. cbn in e.
+      eapply (ih2 (exist p0 e)). assumption.
+    }
+    assert (forall u v p, Acc posR p -> (forall q : pos v, Acc posR q) -> Acc posR (dapp_l u v p)) as Acc_app_l.
+    { intros u v p h ih.
+      induction h as [p ih1 ih2].
+      constructor. intros [q e] h.
+      dependent destruction h.
+      - eapply Acc_app_r with (p := exist p0 e). eapply ih.
+      - eapply (ih2 (exist p0 e)). assumption.
+    }
+    assert (forall indn pr c brs p, Acc posR p -> Acc posR (dcase_c indn pr c brs p))
+      as Acc_case_c.
+    { intros indn pr c brs p h.
+      induction h as [p ih1 ih2].
+      constructor. intros [q e] h.
+      dependent destruction h.
+      eapply (ih2 (exist p0 e)). assumption.
+    }
+    intro t. induction t ; intros q.
+    all: try solve [
+      destruct q as [q e] ; destruct q as [| c q] ; [
+        constructor ; intros [p' e'] h ;
+        unfold posR in h ; cbn in h ;
+        dependent destruction h ;
+        destruct c ; cbn in e' ; discriminate
+      | destruct c ; cbn in e ; discriminate
+      ]
+    ].
+    - destruct q as [q e]. destruct q as [| c q].
+      + constructor. intros [p e'] h.
+        unfold posR in h. cbn in h.
+        dependent destruction h.
+        destruct c ; noconf e'.
+        * eapply Acc_prod_l with (p := exist p0 e').
+          eapply IHt1.
+        * eapply Acc_prod_r with (p := exist p0 e').
+          eapply IHt2.
+      + destruct c ; noconf e.
+        * eapply Acc_prod_l with (p := exist q e).
+          eapply IHt1.
+        * eapply Acc_prod_r with (p := exist q e).
+          eapply IHt2.
+    - destruct q as [q e]. destruct q as [| c q].
+      + constructor. intros [p e'] h.
+        unfold posR in h. cbn in h.
+        dependent destruction h.
+        destruct c ; noconf e'.
+        * eapply Acc_lam_ty with (p := exist p0 e').
+          eapply IHt1.
+        * eapply Acc_lam_tm with (p := exist p0 e').
+          eapply IHt2.
+      + destruct c ; noconf e.
+        * eapply Acc_lam_ty with (p := exist q e).
+          eapply IHt1.
+        * eapply Acc_lam_tm with (p := exist q e).
+          eapply IHt2.
+    - destruct q as [q e]. destruct q as [| c q].
+      + constructor. intros [p e'] h.
+        unfold posR in h. cbn in h.
+        dependent destruction h.
+        destruct c ; noconf e'.
+        * eapply Acc_app_l with (p := exist p0 e').
+          -- eapply IHt1.
+          -- assumption.
+        * eapply Acc_app_r with (p := exist p0 e').
+          eapply IHt2.
+      + destruct c ; noconf e.
+        * eapply Acc_app_l with (p := exist q e).
+          -- eapply IHt1.
+          -- assumption.
+        * eapply Acc_app_r with (p := exist q e).
+          eapply IHt2.
+    - destruct q as [q e]. destruct q as [| c q].
+      + constructor. intros [p' e'] h.
+        unfold posR in h. cbn in h.
+        dependent destruction h.
+        destruct c ; noconf e'.
+        eapply Acc_case_c with (p := exist p0 e').
+        eapply IHt2.
+      + destruct c ; noconf e.
+        eapply Acc_case_c with (p := exist q e).
+        eapply IHt2.
+  Qed.
 
   Notation no := (exist _ false I).
   Notation yes := (exist _ true _).

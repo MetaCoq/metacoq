2db0ac30223f76488a96838a13b51d0233267485
Gregory Malecha <gmalecha@gmail.com>
2019-04-09 19:55:54 -0400

    fixing the checker plugin

---
+++ checker/gen-src/Extract.v
@@ -0,0 +1,9 @@
+From Template Require Import All Extraction.
+
+Cd "../src".
+
+Recursive Extraction Library TypingWf.
+Recursive Extraction Library Checker.
+Recursive Extraction Library Retyping.
+
+Cd "../gen-src".

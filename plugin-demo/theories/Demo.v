2c01af5fcd90a25bd0027b9963c55d15b9ad5cf7
Abhishek Anand (optiplex7010@home) <abhishek.anand.iitg@gmail.com>
2019-04-09 20:58:35 -0700

    ran the lens example, it segfaulted

--- plugin-demo/theories/Demo.v
+++ plugin-demo/theories/Demo.v
@@ -1,2 +1,11 @@
 Require Import Coq.Strings.String.
 Declare ML Module "demo_plugin".
+
+Record Point : Set :=
+  { x: nat;
+    y:nat
+  }.
+
+Make Lens Point.
+(* process coq segmentation fault *)
+    

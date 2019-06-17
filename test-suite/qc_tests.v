36028e25c53ed0af3bedba9fc62f1e1721531c39
Matthieu Sozeau <mattam@mattam.org>
2019-06-17 15:27:34 -0400

    Better pretty-printing and try to generate simple case expressions

--- test-suite/qc_tests.v
+++ test-suite/qc_tests.v
@@ -2,10 +2,125 @@
 
 From Coq Require Import Bool String List Program BinPos Compare_dec Omega.
 From MetaCoq.Template Require Import config Ast AstUtils Induction LiftSubst UnivSubst Typing uGraph utils Checker.
-
+From ExtLib Require Import Monads.
 From QuickChick Require Import QuickChick.
 
-Instance show_term : Show term := { show := string_of_term }.
+Section print_term.
+  Context (Σ : global_context).
+
+  Local Open Scope string_scope.
+
+  Definition string_of_list_aux {A} (f : A -> string) (sep : string) (l : list A) : string :=
+    let fix aux l :=
+        match l return string with
+        | nil => ""
+        | cons a nil => f a
+        | cons a l => f a ++ sep ++ aux l
+        end
+  in aux l.
+
+  Definition print_list {A} (f : A -> string) (sep : string) (l : list A) : string :=
+    string_of_list_aux f sep l.
+
+  Definition parens (top : bool) (s : string) :=
+    if top then s else "(" ++ s ++ ")".
+
+  Definition print_universe_instance u :=
+    match u with
+    | [] => ""
+    | _ => "@{" ++ print_list string_of_level " " u ++ "}"
+    end.
+
+  Definition print_def {A : Set} (f : A -> string) (g : A -> string) (def : def A) :=
+    string_of_name (dname def) ++ " { struct " ++ string_of_nat (rarg def) ++ " }" ++
+                   " : " ++ f (dtype def) ++ " := " ++ nl ++ g (dbody def).
+
+  Definition print_defs (print_term : context -> bool -> term -> string) Γ (defs : mfixpoint term) :=
+    let ctx' := fix_context defs in
+    print_list (print_def (print_term Γ true) (print_term (ctx' ++ Γ)%list true)) (nl ++ " with ") defs.
+
+  Fixpoint print_term (Γ : context) (top : bool) (t : term) {struct t} :=
+  match t with
+  | tRel n =>
+    match nth_error Γ n with
+    | Some {| decl_name := na |} =>
+      match na with
+      | nAnon => "Anonymous (" ++ string_of_nat n ++ ")"
+      | nNamed id => id
+      end
+    | None => "UnboundRel(" ++ string_of_nat n ++ ")"
+    end
+  | tVar n => "Var(" ++ n ++ ")"
+  | tEvar ev args => "Evar(" ++ string_of_nat ev ++ "[]" (* TODO *)  ++ ")"
+  | tSort s => string_of_sort s
+  | tCast c k t => parens top (print_term Γ true c ++ ":"  ++ print_term Γ true t)
+  | tProd na b t =>
+    parens top
+           ("∀ " ++ string_of_name na ++ " : " ++
+                     print_term Γ true b ++ ", " ++ print_term (vass na b :: Γ) true t)
+  | tLambda na b t =>
+    parens top ("λ " ++ string_of_name na ++ " : " ++ print_term Γ true b
+                                ++ " => " ++ print_term (vass na b :: Γ) true t)
+  | tLetIn na b t' t =>
+    parens top ("let" ++ string_of_name na ++ " : " ++ print_term Γ true t' ++
+                      " := " ++ print_term Γ true b ++ " in " ++ nl ++
+                      print_term (vdef na b t' :: Γ) true t)
+  | tApp f l =>
+    parens top (print_term Γ false f ++ " " ++ print_list (print_term Γ false) " " l)
+  | tConst c u => c ++ print_universe_instance u
+  | tInd (mkInd i k) u =>
+    match lookup_ind_decl Σ i k with
+    | Checked (_, oib) => oib.(ind_name) ++ print_universe_instance u
+    | TypeError _ =>
+      "UnboundInd(" ++ string_of_inductive (mkInd i k) ++ "," ++ string_of_universe_instance u ++ ")"
+    end
+  | tConstruct (mkInd i k as ind) l u =>
+    match lookup_ind_decl Σ i k with
+    | Checked (_, oib) =>
+      match nth_error oib.(ind_ctors) l with
+      | Some (na, _, _) => na ++ print_universe_instance u
+      | None =>
+        "UnboundConstruct(" ++ string_of_inductive ind ++ "," ++ string_of_nat l ++ ","
+                            ++ string_of_universe_instance u ++ ")"
+      end
+    | TypeError _ =>
+      "UnboundConstruct(" ++ string_of_inductive ind ++ "," ++ string_of_nat l ++ ","
+                          ++ string_of_universe_instance u ++ ")"
+    end
+  | tCase (mkInd mind i as ind, pars) p t brs =>
+    match lookup_ind_decl Σ mind i with
+    | Checked (_, oib) =>
+      let brs := map (fun x => print_term Γ true (snd x)) brs in
+      let brs := combine brs oib.(ind_ctors) in
+      parens top ("match " ++ print_term Γ true t ++ " in " ++ oib.(ind_name) ++ " return " ++ print_term Γ true p ++
+        " with " ++ print_list (fun '(b, (na, _, _)) => na ++ " => " ++ b) (nl ++ " | ") brs)
+    | TypeError _ =>
+      "Case(" ++ string_of_inductive ind ++ "," ++ string_of_nat i ++ "," ++ string_of_term t ++ ","
+              ++ string_of_term p ++ "," ++ string_of_list (fun b => string_of_term (snd b)) brs ++ ")"
+    end
+  | tProj (mkInd mind i as ind, pars, k) c =>
+    match lookup_ind_decl Σ mind i with
+    | Checked (_, oib) =>
+      match nth_error oib.(ind_projs) k with
+      | Some (na, _) => print_term Γ false c ++ ".(" ++ na ++ ")"
+      | None =>
+        "UnboundProj(" ++ string_of_inductive ind ++ "," ++ string_of_nat i ++ "," ++ string_of_nat k ++ ","
+                       ++ print_term Γ true c ++ ")"
+      end
+    | TypeError _ =>
+      "UnboundProj(" ++ string_of_inductive ind ++ "," ++ string_of_nat i ++ "," ++ string_of_nat k ++ ","
+                     ++ print_term Γ true c ++ ")"
+    end
+
+
+  | tFix l n =>
+    parens top ("let fix " ++ print_defs print_term Γ l ++ nl ++
+                          " in " ++ List.nth_default (string_of_nat n) (map (string_of_name ∘ dname) l) n)
+  | tCoFix l n =>
+    parens top ("let cofix " ++ print_defs print_term Γ l ++ nl ++
+                              " in " ++ List.nth_default (string_of_nat n) (map (string_of_name ∘ dname) l) n)
+  end.
+End print_term.
 
 Definition default_term := tSort Universe.type0m.
 Definition gen_term : G term :=
@@ -86,6 +201,9 @@ Definition term_of_global (gr : global_reference) (u : universe_instance) :=
   | ConstructRef ind n => tConstruct ind n u
   end.
 
+Definition arb_sort : G universe :=
+  elems_ [Universe.type0m; Universe.type0; Universe.type1].
+
 Fixpoint arb (Σ : global_context) (Γ : context) (ty : term) (n : nat) {struct n} : G term :=
   match n with
   | 0 =>
@@ -118,14 +236,17 @@ Fixpoint arb (Σ : global_context) (Γ : context) (ty : term) (n : nat) {struct 
             end
         in
         let globrefs :=
-            (* Valid global references *)
-            map (fun decl =>
-                   match decl with
-                   | ConstantDecl kn cb =>
-                     ConstRef kn
-                   | InductiveDecl kn ib => IndRef (mkInd kn 0)
-                   end)
-                (fst Σ)
+            (* Valid global references: constants, inductives and constructors *)
+            concat (map (fun decl =>
+                           match decl with
+                           | ConstantDecl kn cb => [ConstRef kn]
+                           | InductiveDecl kn mib =>
+                             concat
+                               (mapi (fun i oib =>
+                                let cstrs := mapi (fun j _ => ConstructRef (mkInd kn i) j) oib.(ind_ctors) in
+                                IndRef (mkInd kn i) :: cstrs)
+                                     mib.(ind_bodies))
+                           end) (fst Σ))
         in
         let globrefs :=
             filter (fun gr =>
@@ -152,6 +273,7 @@ Fixpoint arb (Σ : global_context) (Γ : context) (ty : term) (n : nat) {struct 
 
   | S n =>
     (** Generate an application headed by [acc] of type [ty], applying it to [arity] arguments. *)
+    (** Precondition: the type should be normalized (no casts, let or delta reductions needed) *)
     let fix arb_app acc ty arity : G term :=
         match arity with
         | 0 => ret acc
@@ -161,10 +283,10 @@ Fixpoint arb (Σ : global_context) (Γ : context) (ty : term) (n : nat) {struct 
             cand <- arb Σ Γ ty n ;;
             arb_app (mkApp acc cand) (subst10 cand b) arity'
           (* FIXME wrong! Arity doesn't count let-ins *)
-          (*| tLetIn na def def_ty body =>
-        arb_app acc (subst  10 def body) ari      ty'
-        | tCast c _ _ => arb   _app acc c arity *)
-          | _ => oneOf_ []
+          (* | tLetIn na def def_ty body => *)
+          (*   arb_app acc (subst10 def body) ari      ty' *)
+          (* | tCast c _ _ => arb_app acc c arity *)
+          | _ => failGen
           end
         end
     in
@@ -172,15 +294,26 @@ Fixpoint arb (Σ : global_context) (Γ : context) (ty : term) (n : nat) {struct 
         match ty with
         | tProd na ty' b =>
           [body <- arb Σ (vass na ty' :: Γ) b n ;;
-          ret (tProd na ty' body)]
+          ret (tLambda na ty' body)]
         | _ => []
         end
     in
-    let apps : G term :=
-      dom <- arb Σ Γ type_set n ;; (* Generate some set *)
-      f <- arb Σ Γ (tProd nAnon dom (lift0 1 ty)) n;;
-      a <- arb Σ Γ dom n;;
-      ret (tApp f [a])
+    let fix apps i : list (G term) := (* i controls the number of abstractions *)
+      match i with
+      | 0 =>
+        [domu <- arb_sort ;;
+        dom <- arb Σ Γ (tSort domu) n ;; (* Generate some type in the sort *)
+        f <- arb Σ Γ (tProd nAnon dom (lift0 1 ty)) n;;
+        a <- arb Σ Γ dom n;;
+        ret (tApp f [a])]
+      | S i =>
+        (domu <- arb_sort ;;
+        dom <- arb Σ Γ (tSort domu) n ;; (* Generate some type in the sort *)
+        f <- arb Σ Γ (tProd nAnon dom (lift0 1 ty)) n;;
+        a <- arb Σ Γ dom n;;
+        ret (tApp f [a])) :: apps i
+      end
+
     in
     let inverted :=
       (* What should be well-typed by inversion on the type *)
@@ -194,23 +327,51 @@ Fixpoint arb (Σ : global_context) (Γ : context) (ty : term) (n : nat) {struct 
             let ctors :=
              mapi (fun i decl =>
                      let cstrty := type_of_constructor mib decl (mkInd ind k, i) u in
-                     match instantiate_params mib.(ind_params) (List.firstn mib.(ind_npars) args) cstrty with
+                     let params := List.firstn mib.(ind_npars) args in
+                     match instantiate_params mib.(ind_params) params cstrty with
                      | Some cstrty =>
-                       let cstrapp := mkApps (tConstruct (mkInd ind k) i u) args in
+                       let cstrapp := mkApps (tConstruct (mkInd ind k) i u) params in
                        arb_app cstrapp cstrty (snd decl) (* Number of actual arguments *)
-                     | None => oneOf_ []
+                     | None => failGen
                      end)
                   oib.(ind_ctors)
             in
-            oneOf_ ctors
-          | None => oneOf_ [] (* Ill-formed global environment *)
+            ctors
+          | None => [] (* Ill-formed global environment *)
           end
-        | _ => oneOf_ [] (* Ill-formed global environment  *)
+        | _ => [] (* Ill-formed global environment  *)
         end
-      | _ => oneOf_ [] (* Many more possibilities *)
+      | _ => [] (* Many more possibilities *)
       end
     in
-    oneOf_ ([ arb Σ Γ ty (n - n); inverted; apps ] ++ lambdas)
+    let cases : G term :=
+      indty <- arb Σ Γ type_set n;; (** Find some inductive type *)
+      let (indtyhd, args) := decompose_app indty in
+      match indtyhd with
+      | tInd (mkInd ind k) u =>
+        discr <- arb Σ Γ indty n ;;
+        match lookup_mind_decl ind (fst Σ) with
+        | Some mib =>
+          match nth_error mib.(ind_bodies) k with
+          | Some oib =>
+            let params := List.firstn mib.(ind_npars) args in
+            let pred := (tLambda nAnon indty ty) (* Predicate: could be more complicated.
+                                                    FIXME: Ill-formed for inductive families *) in
+            let brtys := map_option_out (build_branches_type (mkInd ind k) mib oib params u pred) in
+            match brtys with
+            | Some tys =>
+              brs <- mapGen (fun '(arity, ty) => t <- arb Σ Γ ty n ;; ret (arity, t)) tys ;;
+              ret (tCase (mkInd ind k, 0) pred discr brs)
+            | None => failGen
+            end
+          | None => failGen
+          end
+        | None => failGen
+        end
+      | _ => failGen
+      end
+    in
+    oneOf_ (arb Σ Γ ty (n - n) :: inverted ++ apps n ++ lambdas ++ [cases])
   end.
 
 Instance check_result {A} : Checkable (typing_result A) :=
@@ -221,16 +382,40 @@ Instance check_result {A} : Checkable (typing_result A) :=
                end) }.
 
 Require Import MetaCoq.Template.Loader.
-Quote Recursively Definition foo := (3 + 4).
+Quote Recursively Definition foo := (3 + 4, @nil bool).
 
+(** Has nat, bool, prod and list *)
 Definition Σ := Eval compute in reconstruct_global_context (fst foo).
+
+Instance show_term : Show term := { show := print_term Σ [] true }.
+
 Definition type_nat := tInd (mkInd "Coq.Init.Datatypes.nat"%string 0) [].
+Definition type_bool := tInd (mkInd "Coq.Init.Datatypes.bool"%string 0) [].
+Definition type_list (A : term) := tApp (tInd (mkInd "Coq.Init.Datatypes.list"%string 0) []) [A].
+Definition type_prod (A B : term) := tApp (tInd (mkInd "Coq.Init.Datatypes.prod"%string 0) []) [A; B].
+Definition add_fn := tConst "Coq.Init.Nat.add"%string [].
+
+Let add_def := Eval compute in match lookup_env Σ "Coq.Init.Nat.add"%string with
+                               | Some (ConstantDecl _ decl) =>
+                                 match decl.(cst_body) with
+                                 | Some body => body
+                                 | None => type_nat
+                                 end
+                               | _ => type_nat
+                               end.
+
+Eval vm_compute in print_term Σ [] true add_def.
 
 Definition arrow ty ty' := tProd nAnon ty (lift0 1 ty').
 
 Sample (arb Σ [] type_set 1).
-Sample (arb Σ [] (arrow type_nat type_nat) 2).
-Sample (arb Σ [] type_nat 10).
+Sample (arb Σ [] type_bool 1).
+Sample (arb Σ [] (arrow type_nat type_nat) 10).
+Sample (arb Σ [] (arrow type_bool type_bool) 3).
+Sample (arb Σ [] type_nat 2).
+Sample (arb Σ [vass (nNamed "n"%string) type_nat] type_nat 1).
+Sample (arb Σ [] type_bool 4).
+Sample (arb Σ [] type_bool 0).
 
 Definition prop_arb_wt :=
   forAll (arb Σ [] type_set 1) (infer Σ []).

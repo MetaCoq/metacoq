587c4e4f2880d032f49d29748bda1cc696c4ba1f
Matthieu Sozeau <mattam@mattam.org>
2019-06-18 18:01:41 -0400

    Force to use bash

--- checker/update_plugin.sh
+++ checker/update_plugin.sh
@@ -1,11 +1,9 @@
+#!/bin/bash
 TEMPLATE_LIB=../template-coq
 
-a=`stat -f "%m" ${TEMPLATE_LIB}/gen-src`
-b=`stat -f "%m" src`
-if [ "$a" -gt "$b" ]
+if [ "src" -ot "${TEMPLATE_LIB}/gen-src" ]
 then
-    echo "${TEMPLATE_LIB} is younger than gen-src"
-
+    echo "${TEMPLATE_LIB} is younger than src"
     cp -r ${TEMPLATE_LIB}/gen-src/to-lower.sh src
     (cd src; ./to-lower.sh)
     cp -r ${TEMPLATE_LIB}/gen-src/*.ml ${TEMPLATE_LIB}/gen-src/*.mli src
--- template-coq/update_plugin.sh
+++ template-coq/update_plugin.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 TOCOPY="ast_denoter.ml ast_quoter.ml denote.ml denoter.ml plugin_core.ml plugin_core.mli quoted.ml quoter.ml run_extractable.ml run_extractable.mli tm_util.ml"
 
 # Test is gen-src is older than src

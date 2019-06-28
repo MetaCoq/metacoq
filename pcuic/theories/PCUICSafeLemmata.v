84f5e073eab3263548007ba3e1fe67e30e7be765
Théo Winterhalter <isheeft@gmail.com>
2019-06-28 11:24:01 -0700

    Complete proof of cumul_zippx

--- pcuic/theories/PCUICSafeLemmata.v
+++ pcuic/theories/PCUICSafeLemmata.v
@@ -473,19 +473,18 @@ Section Lemmata.
       assumption.
     - unfold zippx. simpl.
       eapply cumul_it_mkLambda_or_LetIn. cbn.
-      eapply conv_cumul. eapply conv_Lambda_r.
-      (* Need cumul for Lambda again *)
-      admit.
+      eapply cumul_Lambda_r.
+      assumption.
     - unfold zippx. simpl.
       eapply cumul_it_mkLambda_or_LetIn.
       assumption.
     - unfold zippx. simpl.
       eapply cumul_it_mkLambda_or_LetIn. cbn.
-      (* cumul lambda *)
-      admit.
+      eapply cumul_Lambda_r.
+      assumption.
     - unfold zippx. simpl.
       eapply cumul_it_mkLambda_or_LetIn. assumption.
-  Admitted.
+  Qed.
 
   Lemma conv_zippx :
     forall Γ u v ρ,

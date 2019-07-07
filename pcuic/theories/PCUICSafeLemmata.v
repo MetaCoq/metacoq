a1bfac427aa270e2b08cfd84aa0ddf91c7810564
Théo Winterhalter <isheeft@gmail.com>
2019-07-07 16:30:26 +0200

    Add OnOne2_impl_exist_and_All as a new attempt

--- pcuic/theories/PCUICSafeLemmata.v
+++ pcuic/theories/PCUICSafeLemmata.v
@@ -627,6 +627,29 @@ Section Lemmata.
     - cbn. constructor ; eauto.
   Qed.
 
+  (* TODO MOVE? *)
+  Lemma OnOne2_impl_exist_and_All :
+    forall A (l1 l2 l3 : list A) R1 R2 R3,
+      OnOne2 R1 l1 l2 ->
+      All2 R2 l3 l2 ->
+      (forall x x' y, R1 x y -> R2 x' y -> exists z : A, ∥ R3 x z × R2 x' z ∥) ->
+      exists l4, ∥ OnOne2 R3 l1 l4 × All2 R2 l3 l4 ∥.
+  Proof.
+    intros A l1 l2 l3 R1 R2 R3 h1 h2 h.
+    induction h1 in l3, h2 |- *.
+    - dependent destruction h2.
+      specialize (h _ _ _ p r) as hh.
+      destruct hh as [? [[? ?]]].
+      eexists. constructor. split.
+      + constructor. eassumption.
+      + constructor ; eauto.
+    - dependent destruction h2.
+      specialize (IHh1 _ h2). destruct IHh1 as [? [[? ?]]].
+      eexists. constructor. split.
+      + eapply OnOne2_tl. eassumption.
+      + constructor ; eauto.
+  Qed.
+
   (* TODO MOVE *)
   Lemma red1_eq_term_upto_univ_l :
     forall Re Rle Γ u v u',

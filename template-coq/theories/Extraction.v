e0ef227ed442cbe4ccb43811228862c3f9cec5da
Matthieu Sozeau <mattam@mattam.org>
2019-07-18 18:34:49 -0400

    Fix template-coq compilation: we have two plugins really

---
+++ template-coq/theories/Extraction.v
@@ -0,0 +1,30 @@
+(** Extraction setup for template-coq.
+
+    Any extracted code planning to link with the plugin's OCaml reifier
+    should use these same directives for consistency.
+*)
+
+(* From MetaCoq Require All. *)
+Require Import MetaCoq.Template.utils MetaCoq.Template.Ast.
+Require Import FSets.
+Require Import ExtrOcamlBasic.
+Require Import ExtrOcamlString ExtrOcamlZInt.
+
+(* Ignore [Decimal.int] before the extraction issue is solved:
+   https://github.com/coq/coq/issues/7017. *)
+Extract Inductive Decimal.int => unit [ "(fun _ -> ())" "(fun _ -> ())" ] "(fun _ _ _ -> assert false)".
+
+Extract Constant utils.ascii_compare =>
+ "fun x y -> match Char.compare x y with 0 -> Eq | x when x < 0 -> Lt | _ -> Gt".
+
+Extraction Blacklist config uGraph Universes Ast String List Nat Int
+           UnivSubst Typing Checker Retyping OrderedType Logic Common Equality.
+Set Warnings "-extraction-opaque-accessed".
+
+Cd "gen-src".
+
+Require Import MetaCoq.Template.TemplateMonad.Extractable.
+
+Recursive Extraction Library Extractable.
+
+Cd "..".

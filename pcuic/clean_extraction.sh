9a9146d07705f0baa8ae8e967eecaf18abc53a62
Matthieu Sozeau <mattam@mattam.org>
2019-09-09 15:10:47 +0200

    Deactivate PCUIC plugin installation: another extraction issue

--- pcuic/clean_extraction.sh
+++ pcuic/clean_extraction.sh
@@ -8,21 +8,16 @@ echo "Cleaning result of extraction"
 
 files=`cat ../template-coq/_PluginProject ../checker/_PluginProject.in | grep "^[^#].*mli\?$" | $SED -e s/gen-src/src/`
 
-if [[ -N "src" ]]
-then
-    cd src
-    # Move extracted modules to build the certicoq compiler plugin
-    # Uncapitalize modules to circumvent a bug of coqdep with mllib files
-    for i in *.ml*
-      do
-      newi=`echo $i | cut -b 1 | tr '[:upper:]' '[:lower:]'``echo $i | cut -b 2-`;
-      echo "Moving " $i "to" $newi;
-      mv $i $newi;
-    done
-    cd ..
-else
-    echo "Extracted files are up-to date"
-fi
+cd src
+# Move extracted modules to build the certicoq compiler plugin
+# Uncapitalize modules to circumvent a bug of coqdep with mllib files
+for i in *.ml*
+do
+  newi=`echo $i | cut -b 1 | tr '[:upper:]' '[:lower:]'``echo $i | cut -b 2-`;
+  echo "Moving " $i "to" $newi;
+  mv $i $newi;
+done
+cd ..
 
 # Remove extracted modules already linked in metacoq_template_plugin and checker.
 echo "Removing:" $files

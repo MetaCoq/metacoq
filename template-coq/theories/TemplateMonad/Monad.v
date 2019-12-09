b8c16050c11a505637e416afdf5ad7f8bea1bc48
SimonBoulier <simon.boulier@ens-rennes.fr>
2019-12-09 11:38:03 +0100

    Remove the file Monad.v and use more monadic notation

--- template-coq/theories/TemplateMonad/Monad.v
+++
@@ -1,7 +0,0 @@
-From MetaCoq.Template Require Import TemplateMonad.Core.
-
-(** This allow to use notations of MonadNotation *)
-From MetaCoq.Template Require Import monad_utils.
-
-Instance TemplateMonad_Monad : Monad TemplateMonad :=
-{| ret := @tmReturn ; bind := @tmBind |}.

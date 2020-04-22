8741dcab240df253433836fc8d21e53445348f27
Yannick Forster <forster@ps.uni-saarland.de>
2020-04-22 09:03:38 +0200

    Remove typo

--- pcuic/theories/PCUICCumulativity.v
+++ pcuic/theories/PCUICCumulativity.v
@@ -1,6 +1,6 @@
 (* Distributed under the terms of the MIT license.   *)
 
-From Metgit Coq.Template Require Import config utils.
+From MetaCoq.Template Require Import config utils.
 From MetaCoq.PCUIC Require Import PCUICAst
      PCUICEquality PCUICTyping
      PCUICReduction PCUICPosition.

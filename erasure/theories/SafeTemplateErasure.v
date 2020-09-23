e4fd7934a8e6e18ef6f3b02fee62867de329de2b
Yannick Forster <forster@ps.uni-saarland.de>
2020-09-23 12:21:16 +0200

    port whnf deciders

--- erasure/theories/SafeTemplateErasure.v
+++ erasure/theories/SafeTemplateErasure.v
@@ -1,11 +1,34 @@
 (* Distributed under the terms of the MIT license.   *)
 
-From Coq Require Import Bool String Program.
-From MetaCoq.Template Require Import config monad_utils utils uGraph Pretty.
-From MetaCoq.PCUIC Require Import PCUICAst PCUICAstUtils PCUICTyping
-     TemplateToPCUIC.
-From MetaCoq.SafeChecker Require Import PCUICSafeReduce PCUICSafeChecker
-     SafeTemplateChecker.
+From MetaCoq.Erasure Require Import EAst.
+
+Fixpoint opt_cases (t : term) :=
+  match t with
+  | tBox => tBox
+  | tRel n => tRel n
+  | tVar n => tVar n
+  | tEvar n l => tEvar n (List.map opt_cases l)
+  | tLambda na t => tLambda na (opt_cases t)
+  | tLetIn na t1 t2 => tLetIn na (opt_cases t1) (opt_cases t2)
+  | tApp t1 t2 => tApp (opt_cases t1) (opt_cases t2)
+  | tConst k => tConst k
+  | tConstruct i n => tConstruct i n
+  | tCase p tBox ((a, b) :: _) => mkApps b (List.repeat tBox a)
+  | tCase p t brs => tCase p (opt_cases t) (List.map (fun '(n, t) => (n, opt_cases t)) brs)
+  | tProj p t => tProj p (opt_cases t)
+  | tFix mfix id => tFix (List.map (fun d => Build_def term (dname d) (opt_cases (dbody d)) (rarg d)) mfix) id
+  | tCoFix mfix id => tCoFix (List.map (fun d => Build_def term (dname d) (opt_cases (dbody d)) (rarg d)) mfix) id
+  end.
+
+From Coq Require Import Bool String List Program BinPos Compare_dec Arith Lia.
+From MetaCoq.Template Require Import config monad_utils utils BasicAst AstUtils
+     UnivSubst Pretty.
+From MetaCoq.Checker Require Import uGraph Typing.
+From MetaCoq.PCUIC Require Import PCUICAst PCUICAstUtils PCUICInduction
+     PCUICLiftSubst PCUICUnivSubst PCUICTyping PCUICNormal PCUICSR
+     PCUICGeneration PCUICReflect PCUICEquality PCUICInversion PCUICValidity
+     PCUICWeakening PCUICPosition PCUICCumulativity PCUICSafeLemmata PCUICSN TemplateToPCUIC.
+From MetaCoq.SafeChecker Require Import PCUICSafeReduce PCUICSafeConversion PCUICSafeChecker SafeTemplateChecker.
 From MetaCoq.Erasure Require Import ErasureFunction EPretty.
 From MetaCoq.Erasure Require SafeErasureFunction.
 
@@ -18,11 +41,11 @@ Local Open Scope string_scope.
 
 Program Definition erase_template_program_check (p : Ast.program)
   : EnvCheck (EAst.global_context * EAst.term) :=
-  let Σ := (trans_global (Ast.empty_ext p.1)).1 in
+  let Σ := List.rev (trans_global (AstUtils.empty_ext p.1)).1 in
   G <- check_wf_env Σ ;;
-  Σ' <- wrap_error (empty_ext Σ) "erasure of the global context" (erase_global Σ _) ;;
-  t <- wrap_error (empty_ext Σ) ("During erasure of " ++ string_of_term (trans p.2)) (erase (empty_ext Σ) _ nil _ (trans p.2));;
-  ret (Monad:=envcheck_monad) (Σ', t).
+  Σ' <- wrap_error (PCUICAstUtils.empty_ext Σ) "erasure of the global context" (erase_global Σ _) ;;
+  t <- wrap_error (PCUICAstUtils.empty_ext Σ) ("During erasure of " ++ string_of_term (trans p.2)) (erase (empty_ext Σ) _ nil _ (trans p.2));;
+  ret (Monad:=envcheck_monad) (Σ', opt_cases t).
 
 Next Obligation.
   unfold trans_global.
@@ -59,12 +82,13 @@ Program Fixpoint check_wf_env_only_univs (Σ : global_env)
     G <- check_wf_env_only_univs Σ ;;
     check_fresh d.1 Σ ;;
     let udecl := universes_decl_of_decl d.2 in
-    uctx <- check_udecl (string_of_kername d.1) Σ _ G.π1 (proj1 G.π2) udecl ;;
+    uctx <- check_udecl d.1 Σ _ G.π1 (proj1 G.π2) udecl ;;
     let G' := add_uctx uctx.π1 G.π1 in
     assume_wf_decl (Σ, udecl) _ _ G' _ d.1 d.2 ;;
     match udecl with
         | Monomorphic_ctx _ => ret (G'; _)
         | Polymorphic_ctx _ => ret (G.π1; _)
+        | Cumulative_ctx _ => ret (G.π1; _)
         end
     end.
   Next Obligation.
@@ -126,14 +150,27 @@ Program Fixpoint check_wf_env_only_univs (Σ : global_env)
     rewrite eq1; clear eq1.
     assumption.
   Qed.
+  Next Obligation.
+    split; sq. 2: constructor; tas.
+    unfold global_uctx; simpl.
+    assert (eq1: monomorphic_levels_decl g = LevelSet.empty). {
+      destruct g. destruct c, cst_universes; try discriminate; reflexivity.
+      destruct m, ind_universes; try discriminate; reflexivity. }
+    rewrite eq1; clear eq1.
+    assert (eq1: monomorphic_constraints_decl g = ConstraintSet.empty). {
+      destruct g. destruct c, cst_universes; try discriminate; reflexivity.
+      destruct m, ind_universes; try discriminate; reflexivity. }
+    rewrite eq1; clear eq1.
+    assumption.
+  Qed.
 
 Program Definition erase_template_program (p : Ast.program)
   : EnvCheck (EAst.global_context * EAst.term) :=
-  let Σ := (trans_global (Ast.empty_ext p.1)).1 in
+  let Σ := List.rev (trans_global (AstUtils.empty_ext p.1)).1 in
   G <- check_wf_env_only_univs Σ ;;
   Σ' <- (SafeErasureFunction.erase_global Σ _) ;;
   t <- wrap_error (empty_ext Σ) ("During erasure of " ++ string_of_term (trans p.2)) (SafeErasureFunction.erase (empty_ext Σ) _ nil (trans p.2) _);;
-  ret (Monad:=envcheck_monad) (Σ', t).
+  ret (Monad:=envcheck_monad) (Σ', opt_cases t).
 
 Next Obligation.
   unfold trans_global.
@@ -156,8 +193,8 @@ Program Definition erase_and_print_template_program_check {cf : checker_flags} (
   let p := fix_program_universes p in
   match erase_template_program_check p return string + string with
   | CorrectDecl (Σ', t) =>
-    inl ("Environment is well-formed and " ++ Pretty.print_term (Ast.empty_ext p.1) [] true p.2 ++
-         " erases to: " ++ nl ++ print_term Σ' [] true false t)
+    inl ("Environment is well-formed and " ++ Pretty.print_term (AstUtils.empty_ext p.1) [] true p.2 ++
+         " erases to: " ++ nl ++ EPretty.print_term Σ' [] true false t)
   | EnvError Σ' (AlreadyDeclared id) =>
     inr ("Already declared: " ++ id)
   | EnvError Σ' (IllFormedDecl id e) =>
@@ -170,8 +207,8 @@ Program Definition erase_and_print_template_program {cf : checker_flags} (p : As
   let p := fix_program_universes p in
   match erase_template_program p return string + string with
   | CorrectDecl (Σ', t) =>
-    inl ("Environment is well-formed and " ++ Pretty.print_term (Ast.empty_ext p.1) [] true p.2 ++
-         " erases to: " ++ nl ++ print_term Σ' [] true false t)
+    inl ("Environment is well-formed and " ++ Pretty.print_term (AstUtils.empty_ext p.1) [] true p.2 ++
+         " erases to: " ++ nl ++ EPretty.print_term Σ' [] true false t)
   | EnvError Σ' (AlreadyDeclared id) =>
     inr ("Already declared: " ++ id)
   | EnvError Σ' (IllFormedDecl id e) =>
--- pcuic/theories/PCUICInduction.v
+++ pcuic/theories/PCUICInduction.v
@@ -2,6 +2,10 @@
 
 From MetaCoq.Template Require Import utils.
 From MetaCoq.PCUIC Require Import PCUICAst.
+From MetaCoq.PCUIC Require Import PCUICAst PCUICAstUtils PCUICSize.
+Require Import List Program Lia.
+Require Import BinPos.
+Require Import Coq.Arith.Compare_dec Bool.
 Set Asymmetric Patterns.
 
 (** * Deriving a compact induction principle for terms
@@ -58,4 +62,141 @@ Proof.
   fix auxm 1.
   destruct mfix; constructor; [|apply auxm].
   split; apply auxt.
+Defined.
+
+
+Inductive ForallT {A} (P : A -> Type) : list A -> Type :=
+| ForallT_nil : ForallT P []
+| ForallT_cons : forall (x : A) (l : list A), P x -> ForallT P l -> ForallT P (x :: l).
+
+Definition tCaseBrsType {A} (P : A -> Type) (l : list (nat * A)) :=
+  ForallT (fun x => P (snd x)) l.
+
+Definition tFixType {A : Set} (P P' : A -> Type) (m : mfixpoint A) :=
+  ForallT (fun x : def A => P x.(dtype) * P' x.(dbody))%type m.
+
+Lemma size_decompose_app_rec t L :
+  list_size size L + size t = size (decompose_app_rec t L).1 + list_size size (decompose_app_rec t L).2.
+Proof.
+  induction t in L |- *; cbn; try lia.
+  rewrite <- IHt1. cbn. lia.
+Qed.    
+
+Lemma size_decompose_app t :
+  size t = size (decompose_app t).1 + list_size size (decompose_app t).2.
+Proof.
+  unfold decompose_app.
+  eapply (size_decompose_app_rec t []).
+Qed.
+
+Lemma decompose_app_rec_length_mono t L1 L2 :
+  length L1 <= length L2 ->
+  length (decompose_app_rec t L1).2 <= length (decompose_app_rec t L2).2.
+Proof.
+  intros. induction t in L1, L2, H |- *; cbn; try lia.
+  eapply IHt1. cbn. lia.
+Qed.
+
+Lemma decompose_app_rec_length t L :
+  length (decompose_app_rec t L).2 >= length L.
+Proof.
+  induction t in L |- * ; cbn; try lia.
+  unfold ge. etransitivity. eapply IHt1.
+  eapply decompose_app_rec_length_mono. cbn. lia.
+Qed.
+
+Lemma decompose_app_size_tApp1 t1 t2 :
+  size (decompose_app (tApp t1 t2)).1 < size (tApp t1 t2).
+Proof.
+  rewrite size_decompose_app with (t := tApp t1 t2). cbn.
+  pose proof (decompose_app_rec_length t1 [t2]). cbn in H.
+  pose proof (list_size_length size (decompose_app_rec t1 [t2]).2).
+  lia.
+Qed.
+
+Lemma decompose_app_size_tApp2 t1 t2 :
+  Forall (fun t => size t < size (tApp t1 t2)) (decompose_app (tApp t1 t2)).2.
+Proof.
+  todo String.EmptyString.
+Qed.
+
+Definition mkApps_decompose_app_rec t l :
+  mkApps t l = mkApps  (fst (decompose_app_rec t l)) (snd (decompose_app_rec t l)).
+Proof.
+  revert l; induction t; try reflexivity.
+  intro l; cbn in *.
+  transitivity (mkApps t1 ((t2 ::l))). reflexivity.
+  now rewrite IHt1.
+Qed.
+
+Definition mkApps_decompose_app t :
+  t = mkApps  (fst (decompose_app t)) (snd (decompose_app t))
+  := mkApps_decompose_app_rec t [].
+
+Lemma term_forall_mkApps_ind :
+  forall P : term -> Type,
+    (forall n : nat, P (tRel n)) ->
+    (forall i : ident, P (tVar i)) ->
+    (forall (n : nat) (l : list term), All P l -> P (tEvar n l)) ->
+    (forall s, P (tSort s)) ->
+    (forall (n : name) (t : term), P t -> forall t0 : term, P t0 -> P (tProd n t t0)) ->
+    (forall (n : name) (t : term), P t -> forall t0 : term, P t0 -> P (tLambda n t t0)) ->
+    (forall (n : name) (t : term),
+        P t -> forall t0 : term, P t0 -> forall t1 : term, P t1 -> P (tLetIn n t t0 t1)) ->
+    (forall t : term, forall v, ~ isApp t -> P t -> All P v -> P (mkApps t v)) ->
+    (forall (s : kername) (u : list Level.t), P (tConst s u)) ->
+    (forall (i : inductive) (u : list Level.t), P (tInd i u)) ->
+    (forall (i : inductive) (n : nat) (u : list Level.t), P (tConstruct i n u)) ->
+    (forall (p : inductive * nat) (t : term),
+        P t -> forall t0 : term, P t0 -> forall l : list (nat * term),
+            tCaseBrsProp P l -> P (tCase p t t0 l)) ->
+    (forall (s : projection) (t : term), P t -> P (tProj s t)) ->
+    (forall (m : mfixpoint term) (n : nat), tFixProp P P m -> P (tFix m n)) ->
+    (forall (m : mfixpoint term) (n : nat), tFixProp P P m -> P (tCoFix m n)) ->
+    forall t : term, P t.
+Proof.
+  intros until t.
+  assert (Acc (MR lt size) t) by eapply measure_wf, Wf_nat.lt_wf.
+  induction H. rename X14 into auxt. clear H. rename x into t.
+  move auxt at top.
+
+  destruct t; try now repeat (match goal with
+                 H : _ |- _ => apply H; try (hnf; cbn; lia)
+              end).
+
+  - eapply X1. revert l auxt. unfold MR; cbn. fix auxt' 1.
+    destruct l; constructor. apply auxt. hnf; cbn; lia. apply auxt'. intros. apply auxt.
+    hnf in *; cbn in *. lia. 
+
+  - rewrite mkApps_decompose_app.
+    destruct decompose_app eqn:E. cbn.
+    eapply X6.
+    + eapply decompose_app_notApp in E. eauto.
+    + eapply auxt. cbn. hnf. pose proof (decompose_app_size_tApp1 t1 t2). rewrite E in *. hnf in *; cbn in *. lia.
+    + pose proof (decompose_app_size_tApp2 t1 t2). rewrite E in *. cbn in H. clear E.
+      induction l.
+      * econstructor.
+      * econstructor. eapply auxt. hnf; cbn. inv H. eassumption.
+        eapply IHl. inv H. eassumption.
+        
+  - eapply X10; [apply auxt; hnf; cbn; lia.. | ]. rename brs into l.
+    revert l auxt. unfold MR; cbn. fix auxt' 1.
+    destruct l; constructor. apply auxt. hnf; cbn; lia. apply auxt'. intros. apply auxt.
+    hnf in *; cbn in *. lia. 
+  
+  - eapply X12; [apply auxt; hnf; cbn; lia.. | ]. rename mfix into l.
+    revert l auxt. unfold MR; cbn. fix auxt' 1.
+    destruct l; constructor. split.
+    apply auxt. hnf; cbn. unfold def_size. lia.
+    apply auxt. hnf; cbn. unfold def_size. lia.   
+    apply auxt'. intros. apply auxt.
+    hnf in *; cbn in *. unfold mfixpoint_size, def_size in *. lia. 
+
+  - eapply X13; [apply auxt; hnf; cbn; lia.. | ]. rename mfix into l.
+    revert l auxt. unfold MR; cbn. fix auxt' 1.
+    destruct l; constructor. split.
+    apply auxt. hnf; cbn. unfold def_size. lia.
+    apply auxt. hnf; cbn. unfold def_size. lia.   
+    apply auxt'. intros. apply auxt.
+    hnf in *; cbn in *. unfold mfixpoint_size, def_size in *. lia. 
 Defined.

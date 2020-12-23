45e3e8b8e5bb08818bb3db5e2d346c8e02db5ec2
Matthieu Sozeau <mattam@mattam.org>
2020-12-23 15:47:49 +0100

    Fix replace.sh

--- replace.sh
+++ replace.sh
@@ -2,4 +2,9 @@
 SED=`which gsed || which sed`
 echo $1
 echo $2
-${SED} -i -e "s/${1}/${2}/g" */*/*.v
+
+${SED} -i -E -e \
+    "s/declared_inductive (((\.*\))|([^ _]*)) ([^ ]*) ([^ ]*) ([^ ]*)([\(| ->])/declared_inductive \1 \5 \3 \6\7/g" \
+    */*/*.v
+
+# ${SED} -i -e "s/${1}/${2}/g" */*/*.v

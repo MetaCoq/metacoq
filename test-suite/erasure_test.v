5ef2de12792179b496651017a0733047c278ce19
Matthieu Sozeau <mattam@mattam.org>
2021-11-24 13:19:11 +0100

    Check tests that erasure behaves well

--- test-suite/erasure_test.v
+++ test-suite/erasure_test.v
@@ -45,6 +45,5 @@ Set Warnings "-abstract-large-number".
 Definition bignat := Eval compute in 10000.
 MetaCoq Erase bignat.
 
-Require Import vs.
-(* FIXME unbound rel in erasure! *)
-(* MetaCoq Erase main. *)
+From MetaCoq.TestSuite Require Import vs.
+MetaCoq Erase main.

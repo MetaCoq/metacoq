2598ee45a9e428112d2965bfeb491178cede2c95
Matthieu Sozeau <mattam@mattam.org>
2022-02-11 19:02:33 +0100

    No longer mkdir build

--- template-coq/update_plugin.sh
+++ template-coq/update_plugin.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 SED=`which gsed || which sed`
 TOCOPY="ast_denoter.ml ast_quoter.ml denoter.ml plugin_core.ml plugin_core.mli reification.ml quoter.ml run_extractable.ml run_extractable.mli tm_util.ml"
-SED=`which gsed || which sed`
 
 # Test if gen-src is older than src
 if [[ ! -f "gen-src/denoter.ml" ||
   "theories/Extraction.vo" -nt "gen-src/denoter.ml" || "$1" = "force" ]]
 then
     echo "Updating gen-src from src"
-    mkdir -p build
     echo "Copying from src to gen-src"
     for x in ${TOCOPY}; do rm -f gen-src/$x; cp -a src/$x gen-src/$x; done
     echo "Renaming files to camelCase"

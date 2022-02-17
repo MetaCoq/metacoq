f09c0dbd1e63fef1da267a37646bc49360f308ec
Matthieu Sozeau <mattam@mattam.org>
2022-02-17 10:58:19 +0100

    Revert extraction of nat to int (not compatible with certicoq)

--- template-coq/theories/Extraction.v
+++ template-coq/theories/Extraction.v
@@ -9,7 +9,7 @@ From Coq Require Ascii Extraction ZArith NArith.
 From MetaCoq.Template Require Import utils Ast Reflect Induction.
 From Coq Require Import FSets ExtrOcamlBasic ExtrOcamlString ExtrOCamlFloats
     ExtrOCamlInt63.
-From MetaCoq.Template Require Import MC_ExtrOCamlNatInt MC_ExtrOCamlZPosInt.
+From MetaCoq.Template Require Import MC_ExtrOCamlZPosInt.
 
 Extract Constant ascii_compare =>
  "fun x y -> match Char.compare x y with 0 -> Eq | x when x < 0 -> Lt | _ -> Gt".

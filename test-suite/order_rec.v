428f2ca7231674a7bc77e74aa8e742cd2bbb3114
Matthieu Sozeau <mattam@mattam.org>
2022-02-17 20:53:22 +0100

    Adapt test-suite

--- test-suite/order_rec.v
+++ test-suite/order_rec.v
@@ -2,7 +2,7 @@ From MetaCoq.Template Require Import utils All.
 
 MetaCoq Quote Recursively Definition plus_syntax := plus.
 
-Goal ∑ s1 t1 s2 t2, fst plus_syntax = [(s1, ConstantDecl t1); (s2, InductiveDecl t2)].
+Goal ∑ s1 t1 s2 t2, plus_syntax.1.(declarations) = [(s1, ConstantDecl t1); (s2, InductiveDecl t2)].
 Proof.
   repeat eexists.
 Qed.

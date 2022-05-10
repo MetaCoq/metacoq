d9dc4a3d49d546fe4078c133e63a8e8a5fc2a241
Matthieu Sozeau <mattam@mattam.org>
2022-05-10 18:46:23 +0200

    Remove useless extraction file in template-coq

--- test-suite/loop-checking/theories/Extraction.v
+++ test-suite/loop-checking/theories/Extraction.v
@@ -1,6 +1,9 @@
 Require Import Template.Extraction.
 From MetaCoq.LoopChecking Require Import LoopCheckingPlugin.
 
+Extract Constant BinInt.Z.of_nat => "(fun x -> x)".
+Extract Constant BinInt.Z.to_nat => "(fun x -> x)".
+
 Cd "gen-src".
 
 Extraction Library LoopChecking.

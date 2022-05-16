aac9357f6b863024b3c125a46e4e786750f0ce0a
Kenji Maillard <kenji@maillard.blue>
2022-05-16 11:53:03 +0200

    add support for nix

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+    url = https://github.com/NixOS/nixpkgs/archive/34e4df55664c24df350f59adba8c7a042dece61e.tar.gz;
+    sha256 = "0z313l585blzzj17gnaqi41j3z24gb9ivjdjvqx3r8xwlh0n4b3i";
+  }

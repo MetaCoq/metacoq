22d17dbf99e6aec85e5cbcf18006eb3f58e56fdd
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2022-07-04 12:22:18 +0200

    Release changes (#729)
    * Set version in opam files
    
    * Fix equations dependency
    
    * Fix build.yml
    
    * Fix opam files and add make-opam-files script
    
    * Update version bounds
    
    * Back to 8.16.dev versions

---
+++ make-opam-files.sh
@@ -0,0 +1,23 @@
+#/usr/bin/env bash
+echo "Target directory: " $1    
+echo "Target version: " $2
+echo "Releases package: " $3
+
+wget $3
+archive=`basename $3`
+hash=`shasum -a 512 $archive | cut -f 1 -d " "`
+
+echo "Shasum = " $hash
+
+for f in *.opam; 
+do 
+    opamf=${f/.opam/}; 
+    target=$1/$opamf/$opamf.$2/opam;
+    echo $opamf; 
+    mkdir $1/$opamf/$opamf.$2
+    gsed -e "/^version:.*/d" $f > $target
+    echo url { >> $target
+    echo "  src:" \"$3\" >> $target
+    echo "  checksum:" \"sha512=$hash\" >> $target
+    echo } >> $target
+done

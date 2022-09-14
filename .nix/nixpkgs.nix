7f04168e30571a420927364734687c0a28cad397
Kenji Maillard <kenji@maillard.blue>
2022-09-14 17:43:36 +0200

    trying to fix nix build with an overlay for version 8.16

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/fbac99137c1276ac8aa41d91b4997feaeab256a3.tar.gz;
-    sha256 = "1dkj2b03zxy8c7cp2bs1yjlrg4zwnhb292w6v8h1zav0c3apv2m8";
+    url = https://github.com/NixOS/nixpkgs/archive/61fcca66117a68f7189b93ff199c8147754be14c.tar.gz;
+    sha256 = "1byvy3g4wd6p283z5p6jp8j2fyk1rn7y5a2wis2ja0ws5bymbfv4";
   }

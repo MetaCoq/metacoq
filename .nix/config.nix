8abd90ad69293d7fb42c5e13cab995745e66deb6
Matthieu Sozeau <mattam@mattam.org>
2022-09-20 18:39:03 +0200

    fix

--- .nix/config.nix
+++ .nix/config.nix
@@ -40,8 +40,8 @@
 
     ## You can override Coq and other Coq coqPackages
     ## through the following attribute
-    coqPackages.coq.override.version = "dev";
-    coqPackages.equations.override.version = "dev";
+    coqPackages.coq.override.version = "master";
+    coqPackages.equations.override.version = "master";
 
     ## In some cases, light overrides are not available/enough
     ## in which case you can use either

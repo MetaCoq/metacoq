4aa738ee0e2cc019eab03a373abc073ceaacbe17
Jason Gross <jgross@mit.edu>
2022-10-22 22:01:09 +0530

    Work around https://github.com/coq/coq/issues/16715

--- pcuic/theories/Loader.v
+++ pcuic/theories/Loader.v
@@ -3,5 +3,6 @@ From MetaCoq.Template Require Import Loader.
 From MetaCoq.PCUIC.PCUICTemplateMonad Require Core.
 From MetaCoq.PCUIC Require Import TemplateMonadToPCUIC.
 
-Notation "<% x %>" := (ltac:(let p y := exact y in let p y := run_template_program (@monad_trans y) p in quote_term x p))
+(* Work around COQBUG(https://github.com/coq/coq/issues/16715) *)
+Notation "<% x %>" := (match monad_trans return _ with monad_trans => ltac:(let p y := exact y in let p y := run_template_program (monad_trans y) p in quote_term x p) end)
   (only parsing).

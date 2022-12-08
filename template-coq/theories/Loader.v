b74cedbb07b6d9744dac77176887a0f53ec19e50
Kenji Maillard <kenji@maillard.blue>
2022-12-08 13:34:39 +0100

    fix notation <# _ #>

--- template-coq/theories/Loader.v
+++ template-coq/theories/Loader.v
@@ -7,5 +7,6 @@ Declare ML Module "coq-metacoq-template-coq.plugin".
 Notation "<% x %>" := (ltac:(let p y := exact y in quote_term x p))
   (only parsing).
 
-Notation "<# x #>" := (ltac:(let p y := exact y in run_template_program (TemplateMonad.Core.tmQuoteRec x) p))
-  (only parsing).
+(* Work around COQBUG(https://github.com/coq/coq/issues/16715) with [match] *)
+(* Use [return _] to avoid running the program twice on failure *)
+Notation "<# x #>" := (match TemplateMonad.Core.tmQuoteRec x return _ with qx => ltac:(let p y := exact y in run_template_program qx p) end).

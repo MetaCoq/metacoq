08ab3b6820870e318e33dc1e2155ee444425e8e1
Jason Gross <jgross@mit.edu>
2022-12-15 10:59:36 -0500

    Add `(only parsing)` to `<# x #>` notation
    It's not printable anyway, as it contains `ltac:(...)`

--- template-coq/theories/Loader.v
+++ template-coq/theories/Loader.v
@@ -9,4 +9,5 @@ Notation "<% x %>" := (ltac:(let p y := exact y in quote_term x p))
 
 (* Work around COQBUG(https://github.com/coq/coq/issues/16715) with [match] *)
 (* Use [return _] to avoid running the program twice on failure *)
-Notation "<# x #>" := (match TemplateMonad.Core.tmQuoteRec x return _ with qx => ltac:(let p y := exact y in run_template_program qx p) end).
+Notation "<# x #>" := (match TemplateMonad.Core.tmQuoteRec x return _ with qx => ltac:(let p y := exact y in run_template_program qx p) end)
+  (only parsing).

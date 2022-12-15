0c0bb84b1e7919cb40d8c153e02c879db93af8f3
Jason Gross <jgross@mit.edu>
2022-12-15 11:42:47 -0500

    Uncomment tmMkInductive in PCUIC
    The updated version of #789 managed to define
    `trans_mutual_inductive_entry`

--- pcuic/theories/PCUICTemplateMonad/Core.v
+++ pcuic/theories/PCUICTemplateMonad/Core.v
@@ -12,10 +12,8 @@ Definition tmQuoteRecTransp {A:Type} (a : A) (bypass_opacity : bool) : TemplateM
 Definition tmQuoteInductive (kn : kername) : TemplateMonad mutual_inductive_body := tmQuoteInductive kn.
 Definition tmQuoteConstant (kn : kername) (bypass_opacity : bool) : TemplateMonad constant_body :=
   cb <- tmQuoteConstant kn bypass_opacity;; monad_trans_constant_body cb.
-(*
 Definition tmMkInductive (b : bool) (mie : mutual_inductive_entry) : TemplateMonad unit
-  := tmMkInductive b (PCUICToTemplate.trans_mutual_inductive_entry mie).
- *)
+  := tmMkInductive b (trans_mutual_inductive_entry mie).
 Definition tmUnquote (t : PCUICAst.term) : TemplateMonad typed_term := tmUnquote (PCUICToTemplate.trans t).
 Definition tmUnquoteTyped A (t : PCUICAst.term) : TemplateMonad A := tmUnquoteTyped A (PCUICToTemplate.trans t).
 

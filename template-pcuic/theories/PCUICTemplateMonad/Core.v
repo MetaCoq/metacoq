ee459c1bc59011245a22ab003c39274566f5ccd9
Jason Gross <jgross@mit.edu>
2023-04-08 22:34:53 -0700

    Revert "Don't `tmEval` in translation"
    This reverts commit c0651ce976176a226c0278132406f714dbe1cf63.

--- template-pcuic/theories/PCUICTemplateMonad/Core.v
+++ template-pcuic/theories/PCUICTemplateMonad/Core.v
@@ -11,16 +11,16 @@ Import MCMonadNotation.
 
 Definition tmQuote@{t u} {A:Type@{t}} (a : A) : TemplateMonad@{t u} PCUICAst.term := qa <- tmQuote a;; monad_trans qa.
 Definition tmQuoteRecTransp@{t u} {A:Type@{t}} (a : A) (bypass_opacity : bool) : TemplateMonad@{t u} PCUICProgram.pcuic_program :=
-  (p <- tmQuoteRecTransp a bypass_opacity;; ret (trans_template_program p)).
+  (p <- tmQuoteRecTransp a bypass_opacity;; tmEval cbv (trans_template_program p)).
 Definition tmQuoteInductive@{t u} (kn : kername) : TemplateMonad@{t u} mutual_inductive_body := tmQuoteInductive@{t u} kn.
 Definition tmQuoteConstant@{t u} (kn : kername) (bypass_opacity : bool) : TemplateMonad@{t u} constant_body :=
   cb <- tmQuoteConstant kn bypass_opacity;; monad_trans_constant_body cb.
 Definition tmMkInductive@{t u} (b : bool) (mie : mutual_inductive_entry) : TemplateMonad@{t u} unit
-  := mie <- ret (trans_mutual_inductive_entry mie);; tmMkInductive b mie.
-Definition tmUnquote@{t u} (t : PCUICAst.term) : TemplateMonad@{t u} typed_term := t <- ret (PCUICToTemplate.trans t);; tmUnquote t.
-Definition tmUnquoteTyped@{t u} A (t : PCUICAst.term) : TemplateMonad@{t u} A := t <- ret (PCUICToTemplate.trans t);; tmUnquoteTyped A t.
+  := mie <- tmEval cbv (trans_mutual_inductive_entry mie);; tmMkInductive b mie.
+Definition tmUnquote@{t u} (t : PCUICAst.term) : TemplateMonad@{t u} typed_term := t <- tmEval cbv (PCUICToTemplate.trans t);; tmUnquote t.
+Definition tmUnquoteTyped@{t u} A (t : PCUICAst.term) : TemplateMonad@{t u} A := t <- tmEval cbv (PCUICToTemplate.trans t);; tmUnquoteTyped A t.
 
 (** We keep the original behaviour of [tmQuoteRec]: it quotes all the dependencies regardless of the opaqueness settings *)
 Definition tmQuoteRec {A} (a : A) := tmQuoteRecTransp a true.
 
-Definition tmMkDefinition@{t u} (id : ident) (tm : PCUICAst.term) : TemplateMonad@{t u} unit := tm <- ret (PCUICToTemplate.trans tm);; tmMkDefinition id tm.
+Definition tmMkDefinition@{t u} (id : ident) (tm : PCUICAst.term) : TemplateMonad@{t u} unit := tm <- tmEval cbv (PCUICToTemplate.trans tm);; tmMkDefinition id tm.

8b5049ff2b3727575eea47bafb981c897c25e58a
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2023-11-30 12:07:40 +0100

    Fix translations, and a bit of quotation, up-to an issue with nested inductive definitions.

--- quotation/theories/ToPCUIC/PCUIC/utils/PCUICPrimitive.v
+++ quotation/theories/ToPCUIC/PCUIC/utils/PCUICPrimitive.v
@@ -5,8 +5,8 @@ From MetaCoq.Quotation.ToPCUIC.Common Require Import (hints) Universes Primitive
 
 #[export] Instance quote_array_model {term} {qterm : quotation_of term} {quote_term : ground_quotable term} : ground_quotable (array_model term) := ltac:(destruct 1; exact _).
 
-#[export] Instance quote_prim_model {term tag} {qterm : quotation_of term} : ground_quotable (prim_model term tag) := ltac:(destruct 1; exact _).
+#[export] Instance quote_prim_model {term tag} {qterm : quotation_of term} {quote_term : ground_quotable term} : ground_quotable (prim_model term tag) := ltac:(destruct 1; eauto).
 
-#[export] Instance quote_prim_model_of {term tag} : ground_quotable (prim_model_of term tag) := ltac:(cbv [prim_model_of]; exact _).
+#[export] Instance quote_prim_model_of {term tag} {qterm : quotation_of term} {quote_term : ground_quotable term} : ground_quotable (prim_model_of term tag) := ltac:(cbv [prim_model_of]; destruct tag; exact _).
 
-#[export] Instance quote_prim_val {term} {qterm : quotation_of term} : ground_quotable (prim_val term) := ltac:(cbv [prim_val]; exact _).
+#[export] Instance quote_prim_val {term} {qterm : quotation_of term} {quote_term : ground_quotable term} : ground_quotable (prim_val term) := ltac:(cbv [prim_val]; exact _).

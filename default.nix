43379c28e0e6d12ded2214000626ca7451604e05
4ever2 <3417013+4ever2@users.noreply.github.com>
2024-10-29 22:59:49 +0100

    Add metacoq-translations and fix dependency ordering

--- default.nix
+++ default.nix
@@ -4,9 +4,8 @@
   bundle ? null, job ? null, inNixShell ? null, src ? ./.,
 }@args:
 let auto = fetchGit {
-  url = "https://github.com/4ever2/coq-nix-toolbox.git";
-  allRefs = true ;
-  # ref = "master";
+  url = "https://github.com/coq-community/coq-nix-toolbox.git";
+  ref = "master";
   rev = import .nix/coq-nix-toolbox.nix;
 };
 in
